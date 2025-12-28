bb660909d030cf633dcd21b8aade8dd2ffa7ce30
Daniel Machado <danielmachado20002014@gmail.com>
2025-12-28 15:47:44 -0300

    feat: torna o mapa stateful com GoogleMapController

--- desastres_naturais/lib/pages/map_page.dart
+++ desastres_naturais/lib/pages/map_page.dart
@@ -1,6 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {g
+  static const LatLng _initialPosition =
+      LatLng(-8.0476, -34.8770); // Recife
+
+  GoogleMapController? _mapController;
+
+  @override
+  void dispose() {
+    _mapController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mapa'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: _initialPosition,
+          zoom: 14,
+        ),
+        onMapCreated: (controller) {
+          _mapController = controller;
+        },
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: true,
+      ),
+    );
+  }
+}
+
+
+
+/*
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
@@ -20,28 +66,6 @@ class MapPage extends StatelessWidget {
         ),
         myLocationButtonEnabled: false,
         zoomControlsEnabled: true,
-      ),
-    );
-  }
-}
-
-
-/*
-import 'package:flutter/material.dart';
-
-class MapPage extends StatelessWidget {
-  const MapPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Mapa')),
-      body: const Center(
-        child: Text(
-          'Aqui você verá o mapa com informações úteis.',
-          style: TextStyle(fontSize: 16),
-          textAlign: TextAlign.center,
-        ),
       ),
     );
   }
