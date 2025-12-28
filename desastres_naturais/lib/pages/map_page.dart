993cb9c0b35137dc08a837728bbd719e8301984b
Daniel Machado <danielmachado20002014@gmail.com>
2025-12-28 14:54:16 -0300

    Mapa Google funcionando no Android

--- desastres_naturais/lib/pages/map_page.dart
+++ desastres_naturais/lib/pages/map_page.dart
@@ -1,4 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  static const LatLng _initialPosition =
+      LatLng(-8.0476, -34.8770); // Recife
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mapa'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: _initialPosition,
+          zoom: 14,
+        ),
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: true,
+      ),
+    );
+  }
+}
+
+
+/*
+import 'package:flutter/material.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -17,3 +46,4 @@ class MapPage extends StatelessWidget {
     );
   }
 }
+*/
